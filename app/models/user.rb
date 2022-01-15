a42a6c3acdcc13c8fe1b15a52dfe2939b8995f2b
njguld95038043 <t.tamura6060@gmail.com>
2022-01-15 11:03:01 +0000

    [Update]4-25

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
-
   has_many :books, dependent: :destroy
+
+  def get_profile_image(size)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize: size).processed
+  end
 end
