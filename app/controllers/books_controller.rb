8117c9f2bb675acd31b95bffa375c8201988fcd8
njguld95038043 <t.tamura6060@gmail.com>
2022-01-17 02:42:03 +0000

    [Finish]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -59,7 +59,7 @@ class BooksController < ApplicationController
   def correct_user
     @book = Book.find(params[:id])
     @user = @book.user
-    redirect_to(book_path(@book)) unless @user == current_user
+    redirect_to(books_path) unless @user == current_user
   end
 
 end
